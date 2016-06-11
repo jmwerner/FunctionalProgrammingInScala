d5a7e657433ee836ed20437ae8fb78b9a93db361
Jeremy Werner <jmwerner1123@me.com>
2016-06-10 18:58:11 -0700

    Add final chapter 6 exercises/tests

--- exercises/Chapter6/Driver.scala
+++ exercises/Chapter6/Driver.scala
@@ -3,30 +3,52 @@
 :paste State.scala
 
 // Exercise 1
+
 val simpleRNG = RNG.Simple(1000)
 val (n1, rng2) = RNG.nonNegativeInt(simpleRNG)
 RNG.nonNegativeInt(rng2)
 
 // Exercise 2
+
 RNG.double(rng2)
 
 // Exercise 3
+
 RNG.intDouble(simpleRNG)
 RNG.doubleInt(simpleRNG)
 RNG.double3(simpleRNG)
 
 // Exercise 4
 
+RNG.ints(10)(simpleRNG)
+
 // Exercise 5
+
+RNG.doubleViaMap(rng2)
 
 // Exercise 6
 
+def addTwoInts(a: Int, b: Int): Int = {
+    a + b
+}
+
+RNG.map2(RNG.unit(3), RNG.unit(5))(addTwoInts)
+
 // Exercise 7
+
+val listOfRands = List(RNG.unit(3), RNG.unit(3), RNG.unit(3))
+
+RNG.sequence(listOfRands)
 
 // Exercise 8
 
+RNG.flatMap(RNG.unit(1))(x => RNG.unit(x + 1))
+
 // Exercise 9
 
-// Exercise 10
+def plusTwo(a: Int): Int = {
+    a + 2
+}
 
-// Exercise 11
+RNG._map(RNG.unit(1))(plusTwo)
+
--- exercises/Chapter6/State.scala
+++ exercises/Chapter6/State.scala
@@ -30,15 +30,21 @@ object RNG {
             (f(a), rng2)
         }
 
+    // Exercise 1
+
     def nonNegativeInt(rng: RNG): (Int, RNG) = {
         val (i, r) = rng.nextInt
         (if (i < 0) -(i + 1) else i, r)
     }
 
+    // Exercise 2
+
     def double(rng: RNG): (Double, RNG) = {
         val (i, r) = nonNegativeInt(rng)
         (i / (Int.MaxValue.toDouble + 1), r)
     }
+
+    // Exercise 3
 
     def intDouble(rng: RNG): ((Int,Double), RNG) = {
         val (i, rng1) = rng.nextInt
@@ -46,10 +52,14 @@ object RNG {
         ((i, d), rng2)
     }
 
+    // Exercise 3
+
     def doubleInt(rng: RNG): ((Double,Int), RNG) = {
         val ((i, d), rngOut) = intDouble(rng)
         ((d, i), rngOut)
     }
+
+    // Exercise 3
 
     def double3(rng: RNG): ((Double,Double,Double), RNG) = {
         val (d1, rng1) = double(rng)
@@ -58,23 +68,102 @@ object RNG {
         ((d1, d2, d3), rng3)
     }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+    // Exercise 4
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+    def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+        if (count <= 0) {
+            (List(), rng)
+        } else {
+            val (x, r1)  = rng.nextInt
+            val (xs, r2) = ints(count - 1)(r1)
+            (x :: xs, r2)
+        }
+    }
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+    // Exercise 5
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+    val doubleViaMap: Rand[Double] = {
+        map(nonNegativeInt)(_ / (Int.MaxValue.toDouble + 1))
+    }
+
+    // Exercise 6
+
+    def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+        rng => {
+            val (a, r1) = ra(rng)
+            val (b, r2) = rb(r1)
+            (f(a, b), r2)
+        }
+    }
+
+    // Exercise 7
+
+    def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+        fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
+    }
+
+    // Exercise 8
+
+    def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = {
+        rng => {
+            val (a, r1) = f(rng)
+            g(a)(r1) // We pass the new state along
+        }
+    }
+
+    // Exercise 9
+
+    def _map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+        flatMap(s)(a => unit(f(a)))
+
+}
+
+object State {
+    def unit[S, A](a: A): State[S, A] =
+        State(s => (a, s))
+
+    // Exercise 10
+
+    def sequenceViaFoldRight[S,A](sas: List[State[S, A]]): State[S, List[A]] = {
+        sas.foldRight(unit[S, List[A]](List()))((f, acc) => f.map2(acc)(_ :: _))
+    }
+
+    def sequence[S,A](sas: List[State[S, A]]): State[S, List[A]] = {
+        sequenceViaFoldRight(sas)
+    }
+
+    type Rand[A] = State[RNG, A]
+
 }
 
 case class State[S,+A](run: S => (A, S)) {
-  def map[B](f: A => B): State[S, B] =
-    sys.error("todo")
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    sys.error("todo")
-  def flatMap[B](f: A => State[S, B]): State[S, B] =
-    sys.error("todo")
+
+    def unit[S, A](a: A): State[S, A] =
+        State(s => (a, s))
+
+    // Exercise 10
+
+    def map[B](f: A => B): State[S, B] = {
+        flatMap(a => unit(f(a)))
+    }
+    
+    // Exercise 10
+
+    def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] = {
+        flatMap(a => sb.map(b => f(a, b)))
+    }
+    
+    // Exercise 10
+
+    def flatMap[B](f: A => State[S, B]): State[S, B] = {
+        State(s => {
+            val (a, s1) = run(s)
+            f(a).run(s1)
+        })
+    }
 }
+
+// Exercise 11
 
 sealed trait Input
 case object Coin extends Input
@@ -82,7 +171,19 @@ case object Turn extends Input
 
 case class Machine(locked: Boolean, candies: Int, coins: Int)
 
-object State {
-  type Rand[A] = State[RNG, A]
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+object Candy {
+    def update = {
+        (i: Input) => (s: Machine) =>
+            (i, s) match {
+                case (_, Machine(_, 0, _)) => s
+                case (Coin, Machine(false, _, _)) => s
+                case (Turn, Machine(true, _, _)) => s
+                case (Coin, Machine(true, candy, coin)) =>
+                    Machine(false, candy, coin + 1)
+                case (Turn, Machine(false, candy, coin)) =>
+                    Machine(true, candy - 1, coin)
+            }
+    }
 }
+
+
